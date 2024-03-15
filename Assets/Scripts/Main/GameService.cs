2e2253c467939825c2513e0b26dba410d040b3a9
Yash Sadhukhan <yskhan61@gmail.com>
2024-03-15 18:18:22 +0530

    Use Service Locator
    Add all Services in the game inside Service Locator (GameService.cs)
    Convert all Services into a Non-MonoBehaviour class.
    Define a Constructor for all these services.
    Resolve any dependencies from all of these services through GameService.
    Update references of all services by accessing them through Game Service.

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -1,18 +1,43 @@
+using ServiceLocator.Map;
 using ServiceLocator.Player;
+using ServiceLocator.Sound;
+using ServiceLocator.UI;
 using ServiceLocator.Utilities;
+using ServiceLocator.Wave.Bloon;
+using ServiceLocator.Wave;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ServiceLocator.Events;
 
 public class GameService : GenericMonoSingleton<GameService>
 {
+    [SerializeField] private UIService uiService;
+    public UIService UIService => uiService;
+
     [SerializeField] PlayerScriptableObject playerScriptableObject;
+    [SerializeField] private MapScriptableObject mapScriptableObject;
+    [SerializeField] private WaveScriptableObject waveScriptableObject;
+    
+
+    [SerializeField] private SoundScriptableObject soundScriptableObject;
+    [SerializeField] private AudioSource audioEffects;
+    [SerializeField] private AudioSource backgroundMusic;
 
     public PlayerService PlayerService { get; private set; }
+    public SoundService SoundService { get; private set; }
+    public MapService MapService { get; private set; }
+    public WaveService WaveService { get; private set; }
+    public EventService EventService { get; private set; }
+
 
     private void Start()
     {
+        EventService = new EventService();
         PlayerService = new PlayerService(playerScriptableObject);
+        SoundService = new SoundService(soundScriptableObject, audioEffects, backgroundMusic);
+        MapService = new MapService(mapScriptableObject);
+        WaveService = new WaveService(waveScriptableObject);
     }
 
     private void Update()
